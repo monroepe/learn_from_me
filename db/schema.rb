8cab946d0d4eb28a4d984898c19b6328c49ee86c
Peter <monroepe@gmail.com>
2014-11-20 17:57:57 -0500

    class members created

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20141114212109) do
+ActiveRecord::Schema.define(version: 20141120225006) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "class_members", force: true do |t|
+    t.integer  "user_id",            null: false
+    t.integer  "course_id",          null: false
+    t.text     "qualifications",     null: false
+    t.text     "reason_for_joining"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
 
   create_table "courses", force: true do |t|
     t.string   "name",        null: false
